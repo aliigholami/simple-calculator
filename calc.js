ba30f407f54012fd5f0af4816f8af255b7593c46
aliigholami <im_ali_gholami@proton.me>
2024-06-05 15:24:55 +0330

    calculator created

---
+++ calc.js
@@ -0,0 +1,3 @@
+const clr=()=>{document.getElementById("result").value="";}
+const dis=(number)=>{document.getElementById("result").value+=number;}
+const solve=()=>{document.getElementById("result").value=eval(document.getElementById("result").value);}
